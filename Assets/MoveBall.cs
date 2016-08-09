4a0d235f1b435647e07104587cde24e375818570
Artur Dębkowski <arturdebkowski@gmail.com>
2016-08-09 23:00:42 +0200

    move ball in time

--- Assets/MoveBall.cs
+++ Assets/MoveBall.cs
@@ -3,13 +3,20 @@ using System.Collections;
 
 public class MoveBall : MonoBehaviour {
 
+    // change value in unity
+    public float zSpeed = 10;
+
 	// Use this for initialization
 	void Start () {
+        // log in unity console
         Debug.Log("Hello world");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+        // https://docs.unity3d.com/ScriptReference/Time-deltaTime.html
+        // translate via time
+        float translation = Time.deltaTime * zSpeed;
+        transform.Translate(0, 0, translation);
 	}
 }
