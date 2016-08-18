ce58b8dcd00940237b2f086de52d3840c5800d41
Artur Dębkowski <arturdebkowski@gmail.com>
2016-08-18 23:13:07 +0200

    ReflectOnCollide: on trigger enter

---
+++ Assets/ReflectOnCollide.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ReflectOnCollide : MonoBehaviour {
+
+    //void OnCollisionEnter(Collision collision)
+    //{
+    //    Debug.Log("OnCollisionEnter");
+    //}
+
+    void OnTriggerEnter(Collider collider)
+    {
+        Debug.Log("OnTriggerEnter");
+    }
+}
